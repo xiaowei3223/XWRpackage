a458d305f8a7e988bb0237d4f13eafa632bc1d88
xiaowei3223 <h15016211223@163.com>
2019-11-21 17:54:56 +0800

    first commit

---
+++ R/GPL_biocPackage.R
@@ -0,0 +1,19 @@
+#'  To know what bioconductor package is releted data from GEO database
+#'
+#' You can easily input GPL aceession and return the annotation package names.
+#'
+#' @param GPL  character, one GPL aceession
+#' @return  a character or NA
+
+#' @examples
+#' GPL_biocPackage('GPL20')
+#' GPL_biocPackage('GPL33')
+
+
+GPL_biocPackage = function(GPL){
+  load("data/GPL_bioc_package.RData")
+  pkg = GPL_pkg[which(GPL_pkg$gpl == GPL), ]$bioc_package
+  rm(GPL_pkg)
+  pkg
+}
+
