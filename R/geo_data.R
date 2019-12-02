1ca0fe3e0a1c462aa339c6c9bd1d9b94803595f5
xiaowei3223 <h15016211223@163.com>
2019-12-02 15:09:25 +0800

    修改geo_data函数

--- R/geo_data.R
+++ R/geo_data.R
@@ -4,7 +4,8 @@
 #' @details  if annotation not has your input gene id, then will be only return raw expression data. Also, I should remind you, after mapped gene id, I do not remove those
 #' data not successful map the gene id, which is NA I do not remove.
 #' @param geo   a character, one geo accession
-#' @param geneID  a character, default as "SYMBOL". So you can choose one of "SYMBOL", "ENTREZ" and "RefSeq"
+#' @param geneID  a character, default as "SYMBOL". So you can choose one of "SYMBOL", "ENTREZ" and "RefSeq". If you don't want convert the gene id, this argument set as "NO".
+#'
 #'
 #' @return a list which included expression data, phenotype data, annotation data and GPL number
 #'
@@ -56,26 +57,28 @@ geo_data <- function(geo,geneID = 'SYMBOL'){
 
   #查看平台号注释信息有哪些
   cn_GPL_ann <- colnames(Table(GPL_ann))
+  if(geneID == 'NO'){expr_data <- exp }
+  else{
+    if (geneID == 'SYMBOL'){geneID = "(S|s)(Y|y)(M|m)(B|b)(O|o)(L|l)"}
+    if (geneID == 'ENTREZ'){geneID = "(E|e)(N|n)(T|t)(R|r)(E|e)(Z|z)"}
+    if (geneID == 'RefSeq'){geneID = "(R|r)(e|E)(f|F)(S|s)(e|E)(q|Q)"}
+    #找到含有目标geneID的那一列的名称
+    geneID2 <- grep(geneID, cn_GPL_ann, value = T, ignore.case = FALSE, fixed = FALSE)
 
-  if (geneID == 'SYMBOL'){geneID = "(S|s)(Y|y)(M|m)(B|b)(O|o)(L|l)"}
-  if (geneID == 'ENTREZ'){geneID = "(E|e)(N|n)(T|t)(R|r)(E|e)(Z|z)"}
-  if (geneID == 'RefSeq'){geneID = "(R|r)(e|E)(f|F)(S|s)(e|E)(q|Q)"}
-  #找到含有目标geneID的那一列的名称
-  geneID2 <- grep(geneID, cn_GPL_ann, value = T, ignore.case = FALSE, fixed = FALSE)
-
-  if (length(geneID2) != 0 ){
-    #取对应的想要的平台号信息，比如：SYMBOL
-    iddata <- Table(GPL_ann)[,c('ID', geneID2)]
-    #将探针ID从行名变成一列
-    exp$ID <- rownames(exp)
-    #转换ID
-    expr_data <- merge(x= exp, y=iddata, by = "ID", all.x = T, all.y = F)
-    expr_data1 <- expr_data[,c("ID", geneID2)]
-    expr_data2 <- expr_data[!( names(expr_data) %in% c("ID", geneID2) )]
-    expr_data <- cbind(expr_data1,expr_data2)
-    rm(expr_data1, expr_data2, exp)
-  }else{
-    expr_data <- exp}
+    if (length(geneID2) != 0 ){
+      #取对应的想要的平台号信息，比如：SYMBOL
+      iddata <- Table(GPL_ann)[,c('ID', geneID2)]
+      #将探针ID从行名变成一列
+      exp$ID <- rownames(exp)
+      #转换ID
+      expr_data <- merge(x= exp, y=iddata, by = "ID", all.x = T, all.y = F)
+      expr_data1 <- expr_data[,c("ID", geneID2)]
+      expr_data2 <- expr_data[!( names(expr_data) %in% c("ID", geneID2) )]
+      expr_data <- cbind(expr_data1,expr_data2)
+      rm(expr_data1, expr_data2, exp)
+    }else{
+      expr_data <- exp}
+  }
 
   #返回结果
   geo_exprs_pdata <- list(expr_data, pd, GPL, GPL_ann)
